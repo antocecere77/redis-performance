d04798a9285b6b18f6ba2f4b903b971c8c62903b
antocecere77 <antocecere77@gmail.com>
2021-08-07 11:02:15 +0200

    project creation

---
+++ src/test/java/com/antocecere77/redisperformance/RedisPerformanceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.antocecere77.redisperformance;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RedisPerformanceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
