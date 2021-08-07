009b79ba764b48ad0c471afd8b6841dbf52f6877
antocecere77 <antocecere77@gmail.com>
2021-08-07 12:55:08 +0200

    data setup service - part 1

--- src/main/java/com/antocecere77/redisperformance/entity/Product.java
+++ src/main/java/com/antocecere77/redisperformance/entity/Product.java
@@ -1,11 +1,13 @@
 package com.antocecere77.redisperformance.entity;
 
-import lombok.Data;
-import lombok.ToString;
+import lombok.*;
 import org.springframework.data.annotation.Id;
 
 @Data
 @ToString
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
 public class Product {
 
     @Id
