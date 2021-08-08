71aec0d8a4236ae51a994dab10dcfec0597b29bf
antocecere77 <antocecere77@gmail.com>
2021-08-08 15:55:36 +0200

    abstract cache template

--- src/main/java/com/antocecere77/redisperformance/service/ProductServiceV2.java
+++ src/main/java/com/antocecere77/redisperformance/service/ProductServiceV2.java
@@ -1,4 +1,7 @@
 package com.antocecere77.redisperformance.service;
 
+import org.springframework.stereotype.Service;
+
+@Service
 public class ProductServiceV2 {
 }
---
+++ src/main/java/com/antocecere77/redisperformance/service/util/CacheTemplate.java
@@ -0,0 +1,38 @@
+package com.antocecere77.redisperformance.service.util;
+
+import reactor.core.publisher.Mono;
+
+import java.security.Key;
+
+public abstract class CacheTemplate<KEY, ENTITY> {
+
+    public Mono<ENTITY> get(KEY key) {
+        return getFromCache(key)
+                .switchIfEmpty(
+                        getFromSource(key)
+                        .flatMap(e -> updateCache(key, e))
+                );
+    }
+
+    public Mono<ENTITY> update(KEY key, ENTITY entity) {
+        return updateSource(key, entity)
+                .flatMap(e -> deleteFromCache(key).thenReturn(e));
+    }
+
+    public Mono<Void> delete(KEY key) {
+        return deleteFromSource(key)
+                .then(deleteFromCache(key));
+    }
+
+    abstract protected Mono<ENTITY> getFromSource(KEY key);
+
+    abstract protected Mono<ENTITY> getFromCache(KEY key);
+
+    abstract protected Mono<ENTITY> updateSource(KEY key, ENTITY entity);
+
+    abstract protected Mono<ENTITY> updateCache(KEY key, ENTITY entity);
+
+    abstract protected Mono<Void> deleteFromSource(KEY key);
+
+    abstract protected Mono<Void> deleteFromCache(KEY key);
+}
