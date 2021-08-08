415fe700fcc363aa00c16d3647554b49ba5bc571
antocecere77 <antocecere77@gmail.com>
2021-08-08 16:03:42 +0200

    cache template implementation

---
+++ src/main/java/com/antocecere77/redisperformance/service/util/ProductCacheTemplate.java
@@ -0,0 +1,57 @@
+package com.antocecere77.redisperformance.service.util;
+
+import com.antocecere77.redisperformance.entity.Product;
+import com.antocecere77.redisperformance.repository.ProductRepository;
+import lombok.RequiredArgsConstructor;
+import org.redisson.api.RMapReactive;
+import org.redisson.api.RedissonReactiveClient;
+import org.redisson.codec.TypedJsonJacksonCodec;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import reactor.core.publisher.Mono;
+
+@Service
+public class ProductCacheTemplate extends CacheTemplate<Integer, Product> {
+
+    @Autowired
+    private ProductRepository productRepository;
+
+    private RMapReactive<Integer, Product> map;
+
+    public ProductCacheTemplate(RedissonReactiveClient client) {
+        this.map = client.getMap("product", new TypedJsonJacksonCodec(Integer.class, Product.class));
+    }
+
+    @Override
+    protected Mono<Product> getFromSource(Integer id) {
+        return this.productRepository.findById(id);
+    }
+
+    @Override
+    protected Mono<Product> getFromCache(Integer id) {
+        return this.map.get(id);
+    }
+
+    @Override
+    protected Mono<Product> updateSource(Integer id, Product product) {
+        return this.productRepository.findById(id)
+                .doOnNext(p -> product.setId(id))
+                .flatMap(p -> this.productRepository.save(product));
+    }
+
+    @Override
+    protected Mono<Product> updateCache(Integer id, Product product) {
+        return this.map.fastPut(id, product)
+                .thenReturn(product);
+    }
+
+    @Override
+    protected Mono<Void> deleteFromSource(Integer id) {
+        return this.productRepository.deleteById(id);
+    }
+
+    @Override
+    protected Mono<Void> deleteFromCache(Integer id) {
+        return this.map.fastRemove(id).then();
+    }
+}
